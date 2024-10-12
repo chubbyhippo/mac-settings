0ea00834e729509306d1f0e77ef4e362f81f8398
chubbyhippo <mkmsacc@outlook.com>
2024-10-12 14:22:53 +0700

    Add symlink setup for Emacs configuration
    This change introduces commands to remove any existing .emacs file and create a symbolic link from the .emacs file in the DOTFILES directory to the home directory. This ensures that the Emacs configuration is consistently sourced from the designated DOTFILES location.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ DOTFILES="$(cd "$(dirname "{$0}")" && pwd)"
 rm -rf $HOME/.aerospace.toml
 ln -s $DOTFILES/.aerospace.toml $HOME/.aerospace.toml
 
+# emacs
+rm -rf $HOME/.emacs
+ln -s $DOTFILES/.emacs $HOME/.emacs
+
 # Neovim
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
