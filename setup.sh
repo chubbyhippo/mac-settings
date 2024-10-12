8050dd6e916ac001cdd31bd89a3b3a2b046d95fb
chubbyhippo <mkmsacc@outlook.com>
2024-10-12 14:26:18 +0700

    Update setup.sh to fix symlink paths
    Corrected the symbolic link paths for various config files in setup.sh. This ensures that the configuration files are correctly linked from their respective directories in the DOTFILES folder.

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ DOTFILES="$(cd "$(dirname "{$0}")" && pwd)"
 
 # aerospace
 rm -rf $HOME/.aerospace.toml
-ln -s $DOTFILES/.aerospace.toml $HOME/.aerospace.toml
+ln -s $DOTFILES/aerospace/.aerospace.toml $HOME/.aerospace.toml
 
 # emacs
 rm -rf $HOME/.emacs
-ln -s $DOTFILES/.emacs $HOME/.emacs
+ln -s $DOTFILES/emacs/.emacs $HOME/.emacs
 
 # homebrew
 rm -rf $HOME/Brewfile
-ln -s $DOTFILES/Brewfile $HOME/Brewfile
+ln -s $DOTFILES/homebrew-brew/Brewfile $HOME/Brewfile
 
-# Neovim
+# neovim
 rm -rf $HOME/.config/nvim
-ln -s $DOTFILES/nvim $HOME/.config/nvim
+ln -s $DOTFILES/init.lua $HOME/.config/nvim
