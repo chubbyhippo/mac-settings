d03ceb73989baa09e941d02d624a7f7516e3d225
chubbyhippo <mkmsacc@outlook.com>
2024-10-12 14:24:23 +0700

    Add Homebrew configuration setup
    This change adds commands to remove any existing Homebrew Brewfile in the home directory and symlink it from the $DOTFILES directory. This ensures the Homebrew environment is set up correctly as part of the overall setup script.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ ln -s $DOTFILES/.aerospace.toml $HOME/.aerospace.toml
 rm -rf $HOME/.emacs
 ln -s $DOTFILES/.emacs $HOME/.emacs
 
+# homebrew
+rm -rf $HOME/Brewfile
+ln -s $DOTFILES/Brewfile $HOME/Brewfile
+
 # Neovim
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
